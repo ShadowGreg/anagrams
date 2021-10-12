a111a01debe5cff4512843598a04e8459b66aa1f
Greg Osetrov <shadow-greg@yandex.ru>
2021-10-12 16:37:37 +0300

    Added a new method swapElements, and variable that replaces the space in the break and when printing

--- Anagrams/src/ua/com/foxmainded/anagrams/Anagrams.java
+++ Anagrams/src/ua/com/foxmainded/anagrams/Anagrams.java
@@ -3,76 +3,78 @@ package ua.com.foxmainded.anagrams;
 import java.util.Scanner;
 
 public class Anagrams {
-       
+    
+    public char swapElements(char firstChar, char secondChar) {
+	       return secondChar;
+	   }
 
-    	public String reverseString(String inputString) {
+    public String reverseString(String inputString) {
+	String split = " ";
 
-    		if (inputString == null) {
-    			throw new IllegalArgumentException("String can not be null");
-    		}
-    		String inputWords[] = inputString.split(" ");
-    		String outputWords[] = new String[inputWords.length];
+	if (inputString == null) {
+	    throw new IllegalArgumentException("String can not be null");
+	}
+	String inputWords[] = inputString.split(split);
+	String outputWords[] = new String[inputWords.length];
 
-    		int i = 0;
+	int i = 0;
 
-    		for (String inputWord : inputWords) {
+	for (String inputWord : inputWords) {
 
-    			outputWords[i] = reverseWord(inputWord);
-    			i++;
-    		}
-    		return String.join(" ", outputWords);
-    	}
+	    outputWords[i] = reverseWord(inputWord);
+	    i++;
+	}
+	return String.join(split, outputWords);
+    }
 
-    	private String reverseWord(String inputWord) {
+    private String reverseWord(String inputWord) {
 
-    		char outputWord[] = new char[inputWord.length()];
-    		outputWord = inputWord.toCharArray();
+	char outputWord[] = new char[inputWord.length()];
+	outputWord = inputWord.toCharArray();
 
-    		int beginIndex = 0;
-    		int endIndex = outputWord.length - 1;
+	int beginIndex = 0;
+	int endIndex = outputWord.length - 1;
 
-    		for (int i = 0; i < outputWord.length; i++) {
+	for (int i = 0; i < outputWord.length; i++) {
 
-    			char swappSymbol = outputWord[endIndex];
+	    char swappSymbol = outputWord[endIndex];
 
-    			if (beginIndex < endIndex) {
+	    if (beginIndex < endIndex) {
 
-    				if (!Character.isLetter(outputWord[beginIndex])) {
-    					beginIndex++;
+		if (!Character.isLetter(outputWord[beginIndex])) {
+		    beginIndex++;
 
-    				} else {
-    					if (!Character.isLetter(swappSymbol)) {
-    						endIndex--;
-    						swappSymbol = outputWord[endIndex];
+		} else {
+		    if (!Character.isLetter(swappSymbol)) {
+			endIndex--;
+			swappSymbol = outputWord[endIndex];
 
-    					} else {
-    						outputWord[endIndex] = outputWord[beginIndex];
-    						outputWord[beginIndex] = swappSymbol;
+		    } else {
+			outputWord[endIndex] = swapElements(outputWord[endIndex],
+				outputWord[beginIndex]);
+			outputWord[beginIndex] = swappSymbol;
 
-    						beginIndex++;
-    						endIndex--;
+			beginIndex++;
+			endIndex--;
 
-    						swappSymbol = outputWord[endIndex];
-    					}
-    				}
-    			}
-    		}
-    		return new String(outputWord);
-    	}
-
-
-
+			swappSymbol = outputWord[endIndex];
+		    }
+		}
+	    }
+	}
+	return new String(outputWord);
+    }
 
     public static void main(String[] args) {
 
 	Anagrams reverse = new Anagrams();
-	
+
 	Scanner input = new Scanner(System.in);
-	
+
 	System.out.print("Input a string to reverse: ");
-	
+
 	String inputString = input.nextLine();
-	
+
 	System.out.printf("Reversed string:           " + reverse.reverseString(inputString));
 //	                   Input a string to reverse: 
 	input.close();
